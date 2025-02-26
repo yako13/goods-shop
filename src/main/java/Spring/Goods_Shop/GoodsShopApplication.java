cbf3bb17c1a3bac6a74836cfb9cd12cf533077a9
Yako <parkts4420@naver.com>
2025-02-26 11:50:10 +0900

    git init

---
+++ src/main/java/Spring/Goods_Shop/GoodsShopApplication.java
@@ -0,0 +1,13 @@
+package Spring.Goods_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring/Goods_Shop/GoodsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Goods_Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
