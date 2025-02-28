ee611ce58987ac21945d2bb81e7ba1ba62e35cd2
ranme95 <qkralfks3@naver.com>
2025-02-28 23:37:00 +0900

    Feat : 관리자 주문 상세 내역 페이지 생성

--- src/main/java/Spring/Goods_Shop/Member/MasterController.java
+++ src/main/java/Spring/Goods_Shop/Member/MasterController.java
@@ -3,13 +3,20 @@ package Spring.Goods_Shop.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/master")
 public class MasterController {
 
-    @GetMapping("/master/checkout/list")
+    @GetMapping("/checkout/list")
     String masterCheckoutListPage(){
-        return "master/checkout";
+        return "master/checkout/list";
+    }
+
+    @GetMapping("/checkout/details")
+    String masterCheckoutDetailsPage(){
+        return "master/checkout/details";
     }
 }
