487692b0a851c32909be08fd086908fb8d2449e2
ranme95 <qkralfks3@naver.com>
2025-03-19 21:34:10 +0900

    Feat : 관리자 주문 리스트 sort구현

--- src/main/java/Spring/Goods_Shop/repository/CheckoutRepository.java
+++ src/main/java/Spring/Goods_Shop/repository/CheckoutRepository.java
@@ -4,6 +4,8 @@ import Spring.Goods_Shop.entity.Checkout;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +13,7 @@ public interface CheckoutRepository extends JpaRepository<Checkout,Long> {
     Page<Checkout> findAll(Pageable pageable);
 
     List<Checkout> findAllByMemberId(Long memberId);
+
+    @Query("SELECT c FROM Checkout c WHERE REPLACE(c.member.name, ' ', '') LIKE %:withoutSpaceSearchText%")
+    Page<Checkout> findByMemberNameContainingWithoutSpace(@Param("withoutSpaceSearchText") String name, Pageable pageable);
 }
